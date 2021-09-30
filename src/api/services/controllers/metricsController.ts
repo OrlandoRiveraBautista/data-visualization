2ad7c262bf7788ded0f85d769635ed8004501a62
OrlandoRiveraBautista <47257942+OrlandoRiveraBautista@users.noreply.github.com>
2021-09-29 23:48:46 -0500

    Created all controllers for api queries

--- src/api/services/controllers/metricsController.ts
+++ src/api/services/controllers/metricsController.ts
@@ -1,4 +1,10 @@
 import { gql, ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  GetMeasurementsParams,
+  Measurement,
+  MeasurementQuery,
+  MultipleMeasurements,
+} from '../../../interfaces/measurements';
 
 export class MetricsController {
   private readonly client;
@@ -26,4 +32,75 @@ export class MetricsController {
       });
     return names.getMetrics;
   }
+
+  async getLastKnownMeasurement(name: string): Promise<Measurement> {
+    const dto = this.client.query({
+      query: gql`
+        query {
+          getLastKnownMeasurement(metricName: ${name}) {
+            metric
+            at
+            value
+            unit
+          }
+        }
+      `,
+    });
+    const latestMeasurement: Measurement = await dto
+      .then((res) => res.data.getLastKnownMeasurement)
+      .catch((err) => {
+        throw err;
+      });
+
+    return latestMeasurement;
+  }
+
+  async getMeasurements(input: MeasurementQuery): Promise<Measurement[]> {
+    const dto = this.client.query({
+      query: gql`query{
+        getMeasurements(input: ${input}) {
+          metric
+          at
+          value
+          unit
+        }
+      }`,
+    });
+
+    const givenMeasurements: Measurement[] = await dto
+      .then((res) => res.data.getMeasurements)
+      .catch((err) => {
+        throw err;
+      });
+
+    return givenMeasurements;
+  }
+
+  async getMultipleMeasurements(input: MeasurementQuery): Promise<MultipleMeasurements[]> {
+    const dto = this.client.query({
+      query: gql`query {
+        getMultipleMeasurements(input: ${input}) {
+          metric
+          measurements {
+            metric
+            at
+            value
+            unit
+          }
+        }
+      }`,
+    });
+
+    const multipleMeasurements: MultipleMeasurements[] = await dto
+      .then((res) => res.data.getMultipleMeasurements)
+      .catch((err) => {
+        throw err;
+      });
+
+    return multipleMeasurements;
+  }
+
+  /**
+   *  * Todo: subsciptions to server
+   */
 }
